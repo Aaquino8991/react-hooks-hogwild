be553ce6d2dd2cc45092a6a1c767ca1344fdea60
Anthony <aquinoanthony851@gmail.com>
2023-10-11 11:28:18 -0600

    Hogwild

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
-
+import PigList from "./PigList";
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  const [hogList, setHogList] = useState(hogs);
+  const [greasedOnly, setGreasedOnly] = useState(false); 
+  const [sortOption, setSortOption] = useState("name"); 
+
+  const toggleGreasedFilter = () => {
+    setGreasedOnly(!greasedOnly);
+  };
+
+  const handleSort = (option) => {
+    setSortOption(option);
+  };
+
+  return (
+    <div className="App">
+      <Nav
+        toggleGreasedFilter={toggleGreasedFilter}
+        handleSort={handleSort}
+        sortOption={sortOption}
+        greasedOnly={greasedOnly}
+      />
+      <PigList hogList={hogList} greasedOnly={greasedOnly} sortOption={sortOption} />
+    </div>
+  );
 }
 
 export default App;
